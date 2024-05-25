4e9dde9c956d4f63c83625901ec380091811d4a9
valenari <f.valenari@studenti.unibg.it>
2024-05-25 16:40:54 +0200

    Carte Obiettivo 1

--- CodexNaturalis/src/cardsModel/MazzoObiettivo.java
+++ CodexNaturalis/src/cardsModel/MazzoObiettivo.java
@@ -1,5 +1,40 @@
 package cardsModel;
 
-public class MazzoObiettivo {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class MazzoObiettivo {
+	private List<CartaObiettivo> obiettivi;
+	private int puntatore;
+	
+	// Costruttore del Mazzo di obiettivi + carica le carte da un file
+	public MazzoObiettivo(String filename) {
+		this.obiettivi = new ArrayList<>();
+		this.puntatore = 0;
+		obiettivi.addAll(CartaObiettivo.leggiObiettivi(filename));
+	}
+	
+	// Metodo per pescare una carta dal mazzo
+	public CartaObiettivo pescaObiettivo(){
+		if (puntatore < obiettivi.size()) {
+			return obiettivi.get(puntatore++);
+		}
+		return null; // Se il mazzo è vuoto, ritorna null
+	}
+		
+	// Metodo per mescolare le carte nel mazzo
+	public void mescolaObiettivi() {
+		Collections.shuffle(obiettivi);
+	}
+	
+	// Metodo per ottenere la lista di obiettivi nel mazzo
+	public List<CartaObiettivo> getObiettivi() {
+		return obiettivi;
+	}
+
+	// Getter per il puntatore
+	public int getPuntatore() {
+		return puntatore;
+	}
 }
