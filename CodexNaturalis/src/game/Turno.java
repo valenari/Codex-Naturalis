9bcd94c2ef40e40a65d9e6e770d97ad863771989
p-sanna <p.sanna@studenti.unibg.it>
2024-05-29 21:29:03 +0200

    Classe turno

--- CodexNaturalis/src/game/Turno.java
+++ CodexNaturalis/src/game/Turno.java
@@ -1,5 +1,28 @@
 package game;
 
-public class Turno {
+import Modello_giocatore.Giocatore;
 
+public class Turno {
+		
+	private Giocatore[] giocatori;
+	private int turnoCorrente;
+	
+	public Turno(Giocatore[] giocatori) {
+		this.giocatori = giocatori;
+		this.turnoCorrente = 0;
+	}
+	
+	public Giocatore getGiocatoreCorrente() {
+	     return giocatori[turnoCorrente];
+	}
+
+	public void prossimoTurno() {
+	     turnoCorrente = (turnoCorrente + 1) % giocatori.length;
+	}
+	
+	/*public boolean controlloPunteggio() {
+		if(this.giocatori[turnoCorrente].getPunti()==20) {
+				if()
+		}
+	}*/
 }
